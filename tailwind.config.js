9051549653f258a4939932f370cc0ec3de99bb51
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-07-08 21:03:20 -0400

    setup project with Sanity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      height: {
+        "viewport-height": "calc(min(100vh))",
+      },
+      colors: {
+        primary: {
+          Default: "#F6F6F6",
+          light: "#FFFFFF",
+          darkDefault: "#15141C",
+        },
+        text: {
+          title: "#151E2C",
+          accentBlue: "#0252CD",
+          default: "#9D9D9D",
+        },
       },
     },
   },
   plugins: [],
-}
+};
