05179c971fdfa96876cf0fc1cf3c48a3771ef350
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-07-09 22:34:29 -0400

    create mobile sidebar

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Contact = () => {
   return <div className="h-viewport-height pt-[80px]">Contact Page</div>;
 };
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { FiMoon, FiSun } from "react-icons/fi";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import { rlogo, downloadwhite, downloadblack } from "../../assets/images/index";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+  const [navbarDropdown, setnavbarDropdown] = useState(false);
   return (
-    <div className="fixed backdrop-blur-sm backdrop-filter dark:bg-primary-darkDefault flex flex-row justify-between py-4 md:pl-20 px-4  pr-40 w-full z-50  items-center ">
+    <div className="fixed backdrop-blur-sm backdrop-filter dark:bg-primary-darkDefault flex flex-row justify-between py-4 md:pl-20 px-4  md:pr-40 w-full z-50  items-center ">
       <div>
         <Link href="/">
           <Image
@@ -21,7 +24,8 @@ const Header = () => {
           />
         </Link>
       </div>
-      <div className=" md:flex hidden items-center justify-between gap-6 text-text-navbar dark:text-text-dark font-bold text-base">
+      {/* Desktop view */}
+      <div className=" sm:flex hidden items-center justify-between gap-6 text-text-navbar dark:text-text-dark font-bold text-base">
         <Link
           href="/"
           className="hover:text-text-accentBlue hover:duration-500 transition"
@@ -75,6 +79,70 @@ const Header = () => {
             />
           )}
         </button>
+      </div>
+      {/* MobileView */}
+      <div
+        className="sm:hidden flex relative"
+        onClick={() => setnavbarDropdown((prev) => !prev)}
+      >
+        {navbarDropdown ? <AiOutlineClose /> : <AiOutlineMenu />}
+        {navbarDropdown && (
+          <div className=" absolute top-full right-0 w-full min-w-[150px] shadow-sm shadow-black mt-3 p-4 bg-primary-Default dark:bg-primary-darkDefault flex flex-col items-end rounded-lg  justify-start gap-6 text-text-navbar dark:text-text-dark font-bold text-base ">
+            <Link
+              href="/"
+              className="hover:text-text-accentBlue hover:duration-500 transition"
+            >
+              Home
+            </Link>
+            <Link
+              href="/CaseStudies"
+              className="hover:text-text-accentBlue hover:duration-500 transition"
+            >
+              Case Studies
+            </Link>
+            <Link
+              href="/Contact"
+              className="hover:text-text-accentBlue hover:duration-500 transition"
+            >
+              Contact
+            </Link>
+            <Link
+              href="https://drive.google.com/file/d/1GEyh_LKR5QQJ6yAoAjSsrVpOsRyyZB1r/view?usp=sharing"
+              target="_blank"
+            >
+              <div className="flex flex-row items-center gap-1">
+                <Image
+                  src={theme === "light" ? downloadblack : downloadwhite}
+                  alt="download"
+                  width={100}
+                  height={100}
+                  className="w-5 h-5"
+                />
+                <span className="text-text-title dark:text-white dark:hover:text-text-accentBlue hover:text-text-accentBlue hover:duration-500 transition">
+                  Resume
+                </span>
+              </div>
+            </Link>
+            <button
+              type="button"
+              onClick={() => {
+                setTheme(theme === "light" ? "dark" : "light");
+              }}
+            >
+              {theme === "light" ? (
+                <FiMoon
+                  size={15}
+                  className=" md:w-[25px] md:h-[25px] w-[20px] h-[20px] hover:text-text-accentBlue"
+                />
+              ) : (
+                <FiSun
+                  size={15}
+                  className="md:w-[25px] md:h-[25px] w-[20px] h-[20px] hover:text-primary-highlight"
+                />
+              )}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
