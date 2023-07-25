2aa51d870b4bbb1dddb67683cb324f79487b958f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-07-25 17:32:16 -0400

    create servicecard and service section

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="flex flex-col justify-center items-center pt-5 px-5">
+      <div className="relative inline-block">
+        <span className="z-10 relative text-text-title font-bold xl:text-4xl lg:text-3xl sm:text-xl text-lg dark:text-white">
+          What services do i provide
+        </span>
+        <div className="absolute inset-x-0 -bottom-0.5 bg-primary-highlight md:h-[20px] h-[15px]" />
+      </div>
+      <div>ServiceCard</div>
+    </div>
+  );
 };
 
 export default Services;
