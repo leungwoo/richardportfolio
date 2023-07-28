e7e7861a22314883f969f641a973e51b055b48c0
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-07-28 18:30:48 -0400

    fix footer text size an style underline height

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,13 +8,13 @@ const Footer = () => {
     <div className="flex flex-col items-center justify-center pb-10">
       <div className=" relative">
         <div className=" md:gap-5 gap-3 absolute flex flex-col flex-wrap min-w-[200px] md:top-0 top-2 left-0 right-0 bottom-0 items-center justify-center">
-          <span className="text-xs sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-text-title">
+          <span className="text-sm sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-text-title">
             Have a project idea to{" "}
             <div className="relative inline-block">
-              <span className="z-10 relative text-xs sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl text-text-title font-bold dark:text-white">
+              <span className="z-10 relative text-sm sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl text-text-title font-bold dark:text-white">
                 collaborate on?
               </span>
-              <div className="absolute z-0 inset-x-0 bottom-0 bg-primary-highlight md:h-[20px] h-[15px]"></div>
+              <div className="absolute z-0 inset-x-0 bottom-0 bg-primary-highlight md:h-[20px] h-[10px]"></div>
             </div>
           </span>
 
