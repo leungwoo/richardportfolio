a2c64dd0192763e81c9a27515a487bb48a09d3ac
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-08-04 21:52:46 -0400

    create layout for casestudy projects

--- app/CaseStudies/page.tsx
+++ app/CaseStudies/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Suspense } from "react";
 
 import Loading from "../../components/Loading";
-
 import { Footer, CasestudyGallery } from "../../components";
 import getProjects from "../libs/getProjects";
 
@@ -11,38 +10,35 @@ export default async function CaseStudies() {
   const projectsData = await getProjects();
   console.log(projectsData);
   return (
-    <>
-      <div className="h-viewport-height pt-[80px]">
-        <div className="relative bg-primary-Default dark:bg-primary-carddark  py-[10px] flex items-center justify-center">
-          <div className="absolute flex flex-col flex-wrap md:gap-2 gap-1 top-0 right-0 left-0 bottom-0 text-white items-center justify-center">
-            <h1 className=" text-xl  sm:text-4xl  xl:text-5xl font-bold">
-              Recent Case Studies
-            </h1>
-            <div className="flex flex-col items-center justify-center max-w-[600px] flex-wrap text-center md:px-20 px-14">
-              {" "}
-              <h3 className="lg:text-2xl md:font-normal text-xs">
-                Diving Deep into Our Most Recent Projects: Insights, Challenges,
-                and Triumphs
-              </h3>
-            </div>
-          </div>
-          <div className="flex  md:pb-10 md:px-16 pt-10 pb-5 px-5 ">
-            <Image
-              src={CSbanner}
-              alt="casetstudiesbanner"
-              width={1000}
-              height={500}
-              loading="eager"
-              className="min-w-[286px] lg:max-h-[427px] rounded-[20px] object-cover"
-            />
+    <div className="h-viewport-height pt-[80px]">
+      <div className="relative bg-primary-Default dark:bg-primary-carddark  py-[10px] flex items-center justify-center">
+        <div className="absolute flex flex-col flex-wrap md:gap-2 gap-1 top-0 right-0 left-0 bottom-0 text-white items-center justify-center">
+          <h1 className=" text-xl  sm:text-4xl  xl:text-5xl font-bold">
+            Recent Case Studies
+          </h1>
+          <div className="flex flex-col items-center justify-center max-w-[600px] flex-wrap text-center md:px-20 px-14">
+            {" "}
+            <h3 className="lg:text-2xl md:font-normal text-xs">
+              Diving Deep into Our Most Recent Projects: Insights, Challenges,
+              and Triumphs
+            </h3>
           </div>
         </div>
-        <Suspense fallback={<Loading />}>
-          <CasestudyGallery projectsData={projectsData} />
-        </Suspense>
+        <div className="flex  md:pb-10 md:px-16 pt-10 pb-5 px-5 ">
+          <Image
+            src={CSbanner}
+            alt="casetstudiesbanner"
+            width={1000}
+            height={500}
+            loading="eager"
+            className="min-w-[286px] lg:max-h-[427px] rounded-[20px] object-cover"
+          />
+        </div>
       </div>
-
+      <Suspense fallback={<Loading />}>
+        <CasestudyGallery projectsData={projectsData} />
+      </Suspense>
       <Footer />
-    </>
+    </div>
   );
 }
--- components/CasestudyGallery.tsx
+++ components/CasestudyGallery.tsx
@@ -1,6 +1,7 @@
 import { urlForImage } from "@/sanity/lib/image";
 import Image from "next/image";
 import { Project } from "../config/interfaces";
+import Link from "next/link";
 
 interface Props {
   projectsData: Project[];
@@ -16,26 +17,38 @@ function CasestudyGallery({ projectsData }: Props) {
   ];
   return (
     <div>
-      {projectsData.map((project: Project, index: number) => (
-        <div
-          key={index}
-          style={{ backgroundColor: colors[index % colors.length] }}
-          className="flex flex-col items-center justify-center p-4"
-        >
-          <div className="flex-1 flex items-center justify-center">
-            <Image
-              src={urlForImage(project.imgUrl[1].asset).url()}
-              alt={project.shorttitle}
-              width={1750}
-              height={1080}
-              loading="eager"
-              className="object-contain md:h-[300px] md:w-[500px] w-[280px] h-[200px]"
-            />
+      <div className="flex flex-wrap gap-5 p-5 md:py-10 md:px-20 items-center max-w-[1120px] justify-center mx-auto">
+        {projectsData.map((project: Project, index: number) => (
+          <div
+            key={index}
+            className="flex-1 flex-col items-center justify-center min-w-[350px] "
+          >
+            <Link href={`/project/${project.slug.current}`}>
+              <div
+                className="rounded-2xl pt-10 px-4  hover:shadow-gray-400 hover:shadow-xl transition duration-300 ease-in-out cursor-pointer"
+                style={{ backgroundColor: colors[index % colors.length] }}
+              >
+                <Image
+                  src={urlForImage(project.imgUrl[1].asset).url()}
+                  alt={project.shorttitle}
+                  width={1750}
+                  height={1080}
+                  loading="eager"
+                  className="object-contain "
+                />
+              </div>
+            </Link>
+            <div className="flex flex-col gap-4 mt-5 ml-4">
+              <h1 className="text-[#312E81] dark:text-white font-bold lg:text-[24px]">
+                {project.shorttitle}
+              </h1>
+              <h3 className="text-[#6F74A7] dark:text-[#BEC1D5] font-normal text-[18px]">
+                {project.shortdescription}
+              </h3>
+            </div>
           </div>
-          <h1>{project.shorttitle}</h1>
-          <h3>{project.shortdescription}</h3>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
