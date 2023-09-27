21c8889f84a251607f5fdf6def2f79392f4af73f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-27 15:49:09 -0400

    fix hero layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { urlForImage } from "../sanity/lib/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { FiCopy } from "react-icons/fi";
 
 interface HeroCardProps {
   title: string;
@@ -14,13 +14,9 @@ export default function Hero({ title, description, imgUrl }: HeroCardProps) {
   const titleWords = title.split(" ");
   const [first, second, third] = titleWords;
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: [0, 1] }}
-      className="pt-[80px] flex flex-col bg-primary-Default dark:bg-primary-darkDefault xl:max-h-[794px] md:py-[72px]  py-12  items-center justify-center  w-screen max-w-[1440px]"
-    >
-      <div className="flex md:flex-row flex-col max-h-[750px] items-center pt-10">
-        <div className="flex flex-col md:pl-20 px-4  space-y-5 items-start ">
+    <div className="flex lg:flex-row flex-col pt-[80px] bg-primary-Default dark:bg-primary-darkDefault xl:max-h-[794px] md:py-[72px]  py-12  items-center justify-center  w-screen max-w-[1440px]">
+      <div className="flex flex-col lg:pl-20 px-4 max-h-[750px] md:pt-10">
+        <div className="flex flex-col   space-y-5 items-start ">
           <h3 className="text-sm md:text-base text-text-accentBlue font-bold">
             HI, MY NAME IS RICHARD GABRIEL
           </h3>
@@ -39,25 +35,34 @@ export default function Hero({ title, description, imgUrl }: HeroCardProps) {
           <h4 className="text-text-default dark:text-text-dark leading-9 font-medium text-base">
             {description}
           </h4>
-          <div className="flex flex-row mt-4 items-center ">
-            <Link href="/Contact">
-              <button className="hover:opacity-75 border-none bg-text-accentBlue font-bold dark:bg-primary-darkmodeblue rounded-full sm:w-[186px]  md:h-[64px] w-[330px] h-[55px] py-4 px-3 text-white text-sm md:text-base hover:duration-500 transition">
-                Contact
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-2 pt-5 items-center ">
+          <div className="  items-center">
+            <Link href="/CaseStudies">
+              <button className=" hover:opacity-75 border-none bg-text-accentBlue font-bold dark:bg-primary-darkmodeblue rounded-full md:w-[186px]  md:h-[64px] w-[350px] h-[55px] py-4  text-white text-sm md:text-base hover:duration-500 transition">
+                Case Studies
               </button>
             </Link>
           </div>
-        </div>
-        <div className="flex  xl:h-[500px] min-h-[262px]">
-          <Image
-            src={urlForImage(imgUrl[0])?.url()}
-            alt="hero"
-            width={1000}
-            height={1000}
-            className="xl:h-[500px] min-h-[262px] object-contain "
-            loading="eager"
-          />
+          <div className="flex  ">
+            <button className="flex flex-row justify-between items-center px-4 bg-white hover:opacity-75 border-none rounded-full md:w-[305px]  md:h-[64px] w-[350px] h-[55px] py-[14px] font-semibold text-sm md:text-base hover:duration-500 transition">
+              <span className="text-[#6F74A7]">leungwoo@gmail.com</span>
+              <FiCopy size={25} color="#FFBE62" />
+            </button>
+          </div>
         </div>
       </div>
-    </motion.div>
+      <div className="flex  xl:h-[500px] min-h-[262px] min-w-[393px] lg:pt-10">
+        <Image
+          src={urlForImage(imgUrl[0])?.url()}
+          alt="hero"
+          width={1000}
+          height={1000}
+          className="xl:h-[500px] min-h-[262px] object-contain "
+          loading="eager"
+        />
+      </div>
+    </div>
   );
 }
